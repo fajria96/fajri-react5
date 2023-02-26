62f56e103a5efbef2f39013c4ada80141ff0032a
Fajriatun Munawaroh <fajriatun072@gmail.com>
2023-02-26 18:35:31 +0700

    Create Website Using Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Outlet } from "react-router-dom";
+import Banner from "./components/Banner";
+import NavigationBar from "./components/NavigationBar";
+import "./styles/Route.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="image">
+        <NavigationBar />
+        <Banner />
+      </div>
+      <div className="nav-container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="./Home" className="pages">
+                <strong> Home </strong>
+              </Link>
+            </li>
+            <li>
+              <Link to="./About" className="pages">
+                <strong> About </strong>
+              </Link>
+            </li>
+            <li>
+              <Link to="./Contact" className="pages">
+                <strong> Contact </strong>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
     </div>
   );
 }
